8c98a7e59a9a2dd53527f5b2762adaeb0ac25d32
Kazuki Yamashita <peacesparkl@gmail.com>
2020-07-18 17:12:19 +0900

    user_login add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,18 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'iorinorippp@gmail.com',
+    :password => 'slblhdcbdavxgrpu',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
