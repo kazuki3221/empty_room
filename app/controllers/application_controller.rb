bdd8fb6dce250357c5dd11e61fa651cbd67d1a91
Kazuki Yamashita <peacesparkl@gmail.com>
2020-07-22 11:53:36 +0900

    adddevice:username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,10 @@ class ApplicationController < ActionController::Base
       end
 
   protected
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-      end
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
 
-
+  config.scoped_views = true
   config.mailer_sender = "peacesparkl@gmail.com"
 
   # Configure the class responsible to send e-mails.
