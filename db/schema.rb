e357812ae2fae30c76a040e3844ddadc1e12d70f
Kazuki Yamashita <peacesparkl@gmail.com>
2020-07-22 11:25:26 +0900

    add usernamecolum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_101014) do
+ActiveRecord::Schema.define(version: 2020_07_22_022419) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_101014) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
