fe50c1b1b28fa86e65da09fee0c97c7792604cc4
Kazuki Yamashita <peacesparkl@gmail.com>
2020-07-22 16:44:47 +0900

    reseviton create add

--- app/controllers/resevations_controller.rb
+++ app/controllers/resevations_controller.rb
@@ -5,4 +5,19 @@ class ResevationsController < ApplicationController
 
   def show
   end
+
+  def create
+    @resevation = Resevation.new(resevation_params)
+    if @resevation.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def resevation_params
+      params.require(:resevation).permit(:start_stay, :end_stay)
+    end
 end
