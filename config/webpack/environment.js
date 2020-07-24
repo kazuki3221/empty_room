1a409ac4388b3e104ea5cbe2a064a936e8eac571
Kazuki Yamashita <peacesparkl@gmail.com>
2020-07-24 19:41:17 +0900

    add bootstrap materiaraize

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,11 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
 
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  }))
 
 module.exports = environment
