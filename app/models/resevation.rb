63be3e63dca50f105864ec4729c150965f425233
Kazuki Yamashita <peacesparkl@gmail.com>
2020-08-02 16:37:19 +0900

    resevation should not overlap

--- app/models/resevation.rb
+++ app/models/resevation.rb
@@ -3,6 +3,58 @@ class Resevation < ApplicationRecord
   belongs_to :customer
   belongs_to :user
 
-  validates :start_stay, stay_duplication: true
-  validates :end_stay,   stay_duplication: true
+  validate :start_stay_should_be_before_end_stay
+
+  validate :start_stay_cannot_be_changed
+  validate :end_stay_cannot_be_changed
+ #validates :start_stay, stay_duplication: true
+  #validates :end_stay,   stay_duplication: true
+  validate :resevation_stay_should_not_overlap, if: :new_or_start_or_end_stay_changed
+
+
+private
+def start_stay_should_be_before_end_stay
+  return unless start_stay && end_stay
+
+  if start_stay > end_stay
+    errors.add(:start_stay, 'よりも前に設定してください')
+  end
+end
+
+
+def after_start_stay?
+  start_stay.present? && !new_record? && start_stay_in_database <= Time.zone.now
+end
+
+def start_stay_cannot_be_changed
+  if after_start_stay?
+    if will_save_change_to_start_stay?
+      errors.add(:start_stay, 'は変更できません。宿泊開始日程を過ぎています')
+    end
+  end
+end
+
+def end_stay_cannot_be_changed
+  if after_start_stay?
+    if will_save_change_to_end_stay?
+      errors.add(:end_stay, 'は変更できません。宿泊開始日程を過ぎています')
+    end
+  end
+end
+
+def new_or_start_or_end_stay_changed
+  new_record? || will_save_change_to_start_stay? || will_save_change_to_end_stay?
+end
+
+def resevation_stay_should_not_overlap
+  return unless start_stay && end_stay
+
+  if Resevation.where(room_id: room.id)
+    Resevation.where('start_stay < ?', end_stay)
+    Resevation.where('end_stay > ?', start_stay)
+    Resevation.where.not(id: id).exists?
+    Resevation.where.not(customer_id: customer_id).exists?
+    errors.add(:base, '他の予約と重複しています。')
+    end
+  end
 end
--- app/models/stay_duplication_validator.rb
+++ app/models/stay_duplication_validator.rb
@@ -7,13 +7,16 @@ class StayDuplicationValidator < ActiveModel::EachValidator
 
     return unless new_start_date.present? && new_end_date.present?
 
+
+
      # 重複する期間を検索(編集時は自期間を除いて検索)
     if record.id.present?
-      not_own_periods = StayDuplicationValidator.where('id NOT IN (?) AND start_stay <= ? AND end_stay >= ?', record.id, new_end_date, new_start_date)
+      not_own_periods = Resevation.where('id NOT IN (?) AND start_stay <= ? AND end_stay >= ?', record.id, new_end_date, new_start_date)
     else
-      not_own_periods = StayDuplicationValidator.where('start_stay <= ? AND end_stay >= ?', new_end_date, new_start_date)
+      not_own_periods = Resevation.where('start_stay <= ? AND end_stay >= ?', new_end_date, new_start_date)
     end
 
     record.errors.add(attribute, 'に重複があります') if not_own_periods.present?
   end
+
 end
