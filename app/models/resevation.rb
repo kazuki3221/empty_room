63b251150d0e1aff62598fa4f6f5b8517a4f3638
Kazuki Yamashita <peacesparkl@gmail.com>
2020-08-02 19:36:49 +0900

    resevation valid add

--- app/models/resevation.rb
+++ app/models/resevation.rb
@@ -3,12 +3,14 @@ class Resevation < ApplicationRecord
   belongs_to :customer
   belongs_to :user
 
+  validates :start_stay, presence: true
+  validates :end_stay, presence: true
+
   validate :start_stay_should_be_before_end_stay
+  validate :start_stay_should_be_after_current, if: :new_or_start_stay_changed
 
   validate :start_stay_cannot_be_changed
   validate :end_stay_cannot_be_changed
- #validates :start_stay, stay_duplication: true
-  #validates :end_stay,   stay_duplication: true
   validate :resevation_stay_should_not_overlap, if: :new_or_start_or_end_stay_changed
 
 
@@ -21,6 +23,15 @@ def start_stay_should_be_before_end_stay
   end
 end
 
+def new_or_start_stay_changed
+  start_stay.present? && (new_record? || will_save_change_to_start_stay?)
+end
+
+def start_stay_should_be_after_current
+  if start_stay < Date.today
+    errors.add(:start_stay, 'は前日よりも後に設定してください')
+  end
+end
 
 def after_start_stay?
   start_stay.present? && !new_record? && start_stay_in_database <= Time.zone.now
@@ -49,12 +60,10 @@ end
 def resevation_stay_should_not_overlap
   return unless start_stay && end_stay
 
-  if Resevation.where(room_id: room_id)
-    Resevation.where('start_stay < ?', end_stay)
-    Resevation.where('end_stay > ?', start_stay)
-    Resevation.where.not(id: id).exists?
-    Resevation.where.not(customer_id: customer_id).exists?
-    Resevation.where.not(user_id: user_id).exists?
+  if Resevation.where.not(id: id)
+               .where(room_id: room_id)
+               .where('start_stay <= ?', end_stay)
+               .where('end_stay >= ?', start_stay).exists?
     errors.add(:base, '他の予約と重複しています。')
     end
   end
