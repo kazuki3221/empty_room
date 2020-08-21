49f7c417ba3dd317363a7bc66329a723a33784e3
kazuki3221 <peacesparkl@gmail.com>
2020-08-21 07:56:50 +0000

    mail setting change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "empty_room_production"
 
   config.action_mailer.perform_caching = false
-
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -75,6 +75,20 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # mailer setting
+  config.action_mailer.default_url_options = { host: '54.199.89.136' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => 'kazukioovv@gmail.com',
+    :password => 'blcqvpadncmmcjlk',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
